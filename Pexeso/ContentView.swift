7c9480545cbcecaba5c096fc24ebcaccaf8b0d03
skupen <sv4b@hotmail.com>
2022-08-30 22:45:29 +0200

    Initial Commit

---
+++ Pexeso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pexeso
+//
+//  Created by Petr Skupeň on 30.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pexeso/PexesoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexesoApp.swift
+//  Pexeso
+//
+//  Created by Petr Skupeň on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PexesoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
