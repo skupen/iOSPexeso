71ab3567abd0f2d52c08c020ce932da4d52a04c4
skupen <skupen@outlook.cz>
2022-09-05 23:22:09 +0200

    Adds flipping card functionality

--- Pexeso/ContentView.swift
+++ Pexeso/ContentView.swift
@@ -9,20 +9,48 @@ import SwiftUI
 
 struct PexesoView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-            Text("Hello, world!")
-                .bold()
-                .italic()
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
         }
-        .padding()
         .foregroundColor(.cyan)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text("🚀")
+                    .font(.largeTitle)
+                    .bold()
+                    .italic()
+            } else {
+                shape
+                    .fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         PexesoView()
+            .preferredColorScheme(.light)
+        PexesoView()
+            .preferredColorScheme(.dark)
     }
 }
