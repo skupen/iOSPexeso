e9949b142d2b26c57a6e04bbf6e1fc7b0a1497e7
skupen <skupen@outlook.cz>
2022-09-08 20:21:06 +0200

    Adds iOS app template for the first assignment of the course

---
+++ Pexeso First Assignment/Pexeso First Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pexeso First Assignment
+//
+//  Created by Petr Skupeň on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pexeso First Assignment/Pexeso First Assignment/Pexeso_First_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pexeso_First_AssignmentApp.swift
+//  Pexeso First Assignment
+//
+//  Created by Petr Skupeň on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pexeso_First_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
